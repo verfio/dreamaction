19682c5de18b3f86fb75a6691a0a71cab493ce84
olegchorny <oleg.chorny@gmail.com>
2024-10-02 22:27:01 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24  m-auto ">
       <div className=" pb-10 mx-auto text-center flex flex-col items-start -center max-w-3xl">
         <h1 className=" text-4xl  font-bold  text-gray-900 sm:text-6xl ">
-          Meet Your Personal <span className=" text-blue-600"> Magic the Gathering Assistant </span>
+          Meet Your Personal 
+          <span className=" text-blue-600"> Sparkmage MTG Assistant </span>
           <p className=" mt-10 font-medium   text-lg  max-w-prose text-gray-900 ">
             Deck Ideas, Card Interactions, Financial Insights
           </p>
